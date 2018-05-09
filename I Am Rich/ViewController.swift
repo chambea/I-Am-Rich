e29239160bb0d2adfae2182196d6736e9edc9753
Randy De Silva <randy@Randys-MacBook-Pro.local>
2018-05-08 23:34:31 -0700

    Finished Working App

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Randy De Silva on 5/8/18.
+//  Copyright © 2018 Randy De Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
